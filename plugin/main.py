ea1d6cccab1de2c40ad25956acad89225f3961a2
Leanne Sto. Domingo <65695147+LeanneStoDomingo@users.noreply.github.com>
2025-04-05 00:09:26 -0400

    Add auth with access token

--- plugin/main.py
+++ plugin/main.py
@@ -1,14 +1,47 @@
 from pyflowlauncher import Plugin, Result, ResultResponse, send_results
 from pyflowlauncher.settings import settings
+from pyflowlauncher.api import open_setting_dialog
+import ynab
+
+
+ICO_PATH = "icon.png"
 
 
 plugin = Plugin()
 
+
 @plugin.on_method
 def query(query: str) -> ResultResponse:
-    r = Result(
-        Title=f"This is a title! Your query is {query}",
-        SubTitle="This is the subtitle!",
-        IcoPath="icon.png"
-    )
-    return send_results([r])
+    access_token = settings().get("access_token")
+    if not access_token:
+        return send_results(
+            [
+                Result(
+                    Title="YNAB Personal Access Token is missing",
+                    SubTitle="Click here or press Enter to go to the plugin settings",
+                    IcoPath=ICO_PATH,
+                    JsonRPCAction=open_setting_dialog(),
+                )
+            ]
+        )
+
+    if not query:
+        return send_results([])
+
+    configuration = ynab.Configuration(access_token=access_token)
+
+    with ynab.ApiClient(configuration) as api_client:
+        budgets_api = ynab.BudgetsApi(api_client)
+        budgets_response = budgets_api.get_budgets()
+        budgets = budgets_response.data.budgets
+
+        results = [
+            Result(
+                Title=budget.name,
+                SubTitle="Click to select this budget",
+                IcoPath=ICO_PATH,
+            )
+            for budget in budgets
+        ]
+
+        return send_results(results)
